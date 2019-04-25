ef5fbab9ac3d2133431fecdac35c1820247421ae
Ole Martin Ruud <barskern@outlook.com>
2019-04-25 17:11:39 +0200

    Gjør koden mer pytonisk

--- hdrie/rekonstruksjon/debevec_maliks.py
+++ hdrie/rekonstruksjon/debevec_maliks.py
@@ -6,9 +6,7 @@ med bilder for å kunne gjennskape et bilde med høy dynamisk radians.
 import numpy as np
 
 
-def debevec_maliks(
-    eksp_bilder, eksp_tider, glatthet, antall_pikselverdier, vekter=None
-):
+def debevec_maliks(eksp_bilder, eksp_tider, glatthet, antall_verdier, vekter=None):
     """
     Gitt et sett med pikselverdier observert over flere bilder med ulik
     eksponering, returner responskurven til bildesettet og logaritmen til
@@ -22,17 +20,16 @@ def debevec_maliks(
         Eksponeringstid av bildet med eksponering (j).
     glatthet : float
         Konstant som bestemmer mengden glatthet.
-    antall_pikselverdier : integer
-        Antall ulike pikselverdier (antar at pikselverdier går fra 0 til
-        antall_pikselverdier - 1).
-    vekter : {(antall_pikselverdier,)} ndarray, optional
+    antall_verdier : integer
+        Antall ulike verdier (verdier går fra 0 til antall_verdier - 1).
+    vekter : {(antall_verdier,)} ndarray, optional
         Vektfunksjon som returner et vekttall basert på pikselverdi. Dersom den
         ikke er spesifisert så blir den definert som absoluttverdien av
         avstanden til midten av pikselverdiene.
 
     Returnerer
     ----------
-    g : (antall_pikselverdier,) ndarray
+    g : (antall_verdier,) ndarray
         Responskurven til pikselverdiene.
     lE : {(I,), (X, Y)} ndarray
         Logaritmen til irradiansen til pikslen ved gitt posisjon.
@@ -53,13 +50,13 @@ def debevec_maliks(
     elif len(eksp_bilder.shape) < 2 or 3 < len(eksp_bilder.shape):
         raise ValueError("eksp_bilder må ha enten 2 eller 3 dimensjoner")
 
-    antall_verdier = eksp_bilder.shape[1]
+    antall_piksler = eksp_bilder.shape[1]
 
     if vekter is None:
         vekter = np.concatenate(
             (
-                np.arange(1, 1 + antall_pikselverdier // 2),
-                np.arange(antall_pikselverdier // 2, 0, -1),
+                np.arange(1, 1 + antall_verdier // 2),
+                np.arange(antall_verdier // 2, 0, -1),
             )
         )
 
@@ -70,50 +67,56 @@ def debevec_maliks(
 
     A = np.zeros(
         (
-            antall_eksp * antall_verdier + antall_pikselverdier - 1,
-            antall_pikselverdier + antall_verdier,
+            antall_eksp * antall_piksler + antall_verdier - 1,
+            antall_verdier + antall_piksler,
         )
     )
     b = np.zeros(A.shape[0])
 
-    # Sett inn de data-tilpassende funksjonene.
-    k = 0
-    # For hver eksponering av bildet og tilhørende eksponeringstid.
-    for eksp, piksler in zip(log_eksp_tider, eksp_bilder):
-        # For hver pikselverdi og tilhørende indeks.
-        for i, verdi in enumerate(piksler):
-            A[k, verdi] = vekter[verdi]
-            A[k, antall_pikselverdier + i] = -vekter[verdi]
+    # Indeksen til irradiansene i likningssettet.
+    irradians_indeks = np.arange(antall_piksler) + antall_verdier
 
-            b[k] = vekter[verdi] * eksp
+    # Indeksen inn i matrisen for likningene.
+    k = np.arange(antall_piksler)
 
-            k += 1
+    # For hver eksponering av bildet og tilhørende logaritmisk eksponeringstid.
+    for ln_eksp, piksler in zip(log_eksp_tider, eksp_bilder):
+        # Alle responskurvene for disse plasseringene har `w(z)`.
+        A[k, piksler] = vekter[piksler]
+
+        # Alle irradiansene for disse plasseringene har `-w(z)`.
+        A[k, irradians_indeks] = -vekter[piksler]
+
+        # Alle tidene for disse plasseringene har `w(z) * ln dt`.
+        b[k] = vekter[piksler] * ln_eksp
+
+        # Inkrementer til neste del av matrisen for neste eksponering.
+        k += antall_piksler
 
     # Fiks kurven ved å sette midtpunktet til 0.
-    A[k, antall_pikselverdier // 2] = 1
-    k += 1
+    A[antall_eksp * antall_piksler, antall_verdier // 2] = 1
+
+    # Lag indeksering for alle verdier (eksl. ytterpunkter) i 3 dimensjoner.
+    i = np.arange(1, antall_verdier - 1)[:, np.newaxis].repeat(3, axis=1)
+
+    # Endre `k` til å indeksere over alle verdier (utenom ytterpunkter) med
+    # offsett fra tidligere definert likning.
+    k = i + (antall_piksler * antall_eksp)
 
     # Sørg for at kurven er glatt ved å legge til ledd som prøver å minimere
     # g''(z), altså få kurven mest mulig glatt.
-    for i in range(1, antall_pikselverdier - 1):
-        A[k, i] = -2 * glatthet * vekter[i]
-        A[k, i - 1] = A[k, i + 1] = glatthet * vekter[i]
-
-        k += 1
+    A[k, i + [-1, 0, 1]] = vekter[i] * glatthet * [1, -2, 1]
 
     # Løs likningssettet.
     x = np.linalg.lstsq(A, b, rcond=None)[0]
 
-    return (
-        x[:antall_pikselverdier],
-        x[antall_pikselverdier:].reshape(orginal_verdi_form),
-    )
+    return x[:antall_verdier], x[antall_verdier:].reshape(orginal_verdi_form)
 
     # end snippet debevec-maliks-algo
 
 
 def rekonstruer_irradians(
-    eksp_bilder, eksp_tider, responskurve, antall_pikselverdier, vekter=None
+    eksp_bilder, eksp_tider, responskurve, antall_verdier, vekter=None
 ):
     """
     Gitt et sett med pikselverdier observert over flere bilder med ulik
@@ -126,12 +129,12 @@ def rekonstruer_irradians(
         Ulike eksponeringer (j) av pikselverdier (i) eller (x, y).
     eksp_tider : (E,) ndarray
         Eksponeringstid av bildet med eksponering (j).
-    responskurve : (antall_pikselverdier,) ndarray
+    responskurve : (antall_verdier,) ndarray
         Responskurven til bildene.
-    antall_pikselverdier : integer
+    antall_verdier : integer
         Antall ulike pikselverdier (antas at pikselverdier går fra 0 til
-        antall_pikselverdier - 1).
-    vekter : {(antall_pikselverdier,)} ndarray, optional
+        antall_verdier - 1).
+    vekter : {(antall_verdier,)} ndarray, optional
         Vektfunksjon som returner et vekttall basert på pikselverdi. Dersom den
         ikke er spesifisert så blir den definert som absoluttverdien av
         avstanden til midten av pikselverdiene.
@@ -144,8 +147,8 @@ def rekonstruer_irradians(
     if vekter is None:
         vekter = np.concatenate(
             (
-                np.arange(1, 1 + antall_pikselverdier // 2),
-                np.arange(antall_pikselverdier // 2, 0, -1),
+                np.arange(1, 1 + antall_verdier // 2),
+                np.arange(antall_verdier // 2, 0, -1),
             )
         )
 
